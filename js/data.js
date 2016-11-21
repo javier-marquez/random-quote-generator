078b984b40f18155952d1e6663f921fedba2fed0
javier marquez <exa.dev@outlook.com>
2016-11-21 19:31:59 +0100

    Uploading the quote generator
    A site that generates random quotes using javascript, avoiding hard
    coded html

---
+++ js/data.js
@@ -0,0 +1,163 @@
+var quotes = [{
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "If you are distressed by anything external, the pain is not due to the thing itself, but to your estimate of it; and this you have the power to revoke at any moment.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Erin Hunter",
+    citation: "Into the Wild ",
+    quote: "Warriors should suffer their pain silently.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 1996
+}, {
+    author: "Randy Pausch",
+    citation: "The Last Lecture",
+    quote: "Complaining does not work as a strategy. We all have finite time and energy. Any time we spend whining is unlikely to help us achieve our goals. And it won't make us happier.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 2008
+}, {
+    author: "Seneca",
+    citation: "The Stoic Philosophy of Seneca: Essays and Letters ",
+    quote: "It is the power of the mind to be unconquerable.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Nora Roberts",
+    citation: "Midnight Bayou",
+    quote: "Feeling too much is a hell of a lot better than feeling nothing.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 2001
+}, {
+    author: " Jane Austen",
+    citation: "Sense and Sensibility",
+    quote: "Always resignation and acceptance. Always prudence and honour and duty. Elinor, where is your heart? ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 1811
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: "If you really want to escape the things that harass you, what you’re needing is not to be in a different place but to be a different person.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: " Enjoy present pleasures in such a way as not to injure future ones. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: "Regard [a friend] as loyal, and you will make him loyal.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: "Nothing is burdensome if taken lightly, and nothing need arouse one's irritation so long as one doesn't make it bigger than it is by getting irritated. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: "Of this one thing make sure against your dying day - that your faults die before you do. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: " You should … live in such a way that there is nothing which you could not as easily tell your enemy as keep to yourself. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Seneca",
+    citation: "Letters from a Stoic",
+    quote: " To win true freeedom you must be a slave to philosophy. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 65
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "Dwell on the beauty of life. Watch the stars, and see yourself running with them.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "You have power over your mind - not outside events. Realize this, and you will find strength. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "The happiness of your life depends upon the quality of your thoughts. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: "Waste no more time arguing about what a good man should be. Be one.",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Marcus Aurelius",
+    citation: "Meditations",
+    quote: " Very little is needed to make a happy life; it is all within yourself in your way of thinking. ",
+    tags: ["Stoicism", "Philosophy", "Mind"],
+    year: 180
+}, {
+    author: "Friedrich August von Hayek",
+    citation: "The Road to Serfdom",
+    quote: "Emergencies have always been the pretext on which the safeguards of individual liberty have been eroded.",
+    tags: ["Freedom", "Liberty", "Individual"],
+    year: 1944
+}, {
+    author: "Friedrich August von Hayek",
+    citation: "The Road to Serfdom",
+    quote: "A claim for equality of material position can be met only by a government with totalitarian powers.",
+    tags: ["Equality", "Government"],
+    year: 1944
+}, {
+    author: "Friedrich August von Hayek",
+    citation: "The Road to Serfdom",
+    quote: "Freedom granted only when it is known beforehand that its effects will be beneficial is not freedom.",
+    tags: ["Freedom", "Will", "Only"],
+    year: 1944
+}, {
+    author: "Friedrich August von Hayek",
+    citation: "The Road to Serfdom",
+    quote: "We must face the fact that the preservation of individual freedom is incompatible with a full satisfaction of our views of distributive justice.",
+    tags: ["Freedom", "Justice", "Face"],
+    year: 1944
+}, {
+    author: "Wayne W. Dyer",
+    citation: "Your Erroneous Zones",
+    quote: "A truer barometer of intelligence is an effective, happy life lived each day and each present moment of every day.",
+    tags: ["Happiness", "Intelligence", "Wisdom"],
+    year: 1976
+}, {
+    author: "Vladimir Bukovsky",
+    citation: "Soviet Hypocrisy and Western Gullibility",
+    quote: "Socialism is the gradual and less violent form of communism, and the project of the European Union... The intent was to save socialism in Europe after the fall of the Berlin Wall and the predictable bankruptcy of the welfare state in the West as well.",
+    tags: ["Economics", "Socialism", "Goverment"],
+    year: 1977
+}, {
+    author: "Winston Churchill",
+    citation: "An iron curtain has descended",
+    quote: "Socialism is a philosophy of failure, the creed of ignorance, and the gospel of envy, its inherent virtue is the equal sharing of misery.",
+    tags: ["Socialism", "Ignorance"],
+    year: 1952
+}, {
+    author: "Milton Friedman",
+    citation: "Free to choose",
+    quote: "The greatest advances of civilization, have never come from centralized government.*",
+    tags: ["Economics", "Goverment", "Technology"],
+    year: 1980
+}];
---
+++ js/script.js
@@ -0,0 +1,139 @@
+/*
+Overriding the remove method
+http://stackoverflow.com/questions/3387427/remove-element-by-id
+*/
+
+Element.prototype.remove = function () {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+
+
+var selectedQuote; //The quote object is initialized
+var keysName = []; //the array with the object keys
+var shownQuotes = []; //quotes that have been shown are stored here so that we dont repeat them
+
+
+
+//Sets the background-color of the specified dom element to the range : rgba(49, 46, 127, 0.9) - rgba(49, 46, 252, 0.9)
+//it is a blueish color
+function randomBlueish(elementId) {
+    var randomBlue = Math.floor((Math.random() * 125) + 127)
+
+    var rgbaValue = "rgba(49, 46, ";
+    rgbaValue += randomBlue;
+    rgbaValue += ", 0.9)";
+
+    document.getElementById(elementId).style.backgroundColor = rgbaValue;
+
+}
+
+
+//returns random object from an arrray
+function getRandomQuote(arr) {
+    var i = Math.floor((Math.random() * arr.length));
+    var randomObject = arr[i];
+    return randomObject;
+}
+
+/*retreives a random object form the array, deletes it, and adds it to a list
+of shown objects, it avoids duplicates until all quotes are shown*/
+function filterQuote() {
+
+    //Select quote
+    if (quotes.length >= 1) {
+        //from quotes
+        selectedQuote = getRandomQuote(quotes);
+        //capture it inside shownquotes array
+        shownQuotes.push(selectedQuote);
+        //eliminate it from quotes
+        quotes.pop(selectedQuote);
+
+    } else {
+        //if there are no more quotes available get the already used
+        selectedQuote = getRandomQuote(shownQuotes);
+    }
+
+    //get all the needed keys
+    for (var key in selectedQuote) {
+        keysName.push(key);
+    }
+
+
+    //log to the console
+    console.log(selectedQuote);
+}
+
+function displayQuote() {
+    for (var index = 0; index < keysName.length; index++) {
+        var key = keysName[index];
+
+        var content;
+
+        if (selectedQuote[key]) {
+
+            if (selectedQuote[key].constructor === Array) {
+                //join the array content
+                content = selectedQuote[key].join(' - ');
+            } else {
+                content = selectedQuote[key];
+            }
+
+
+        }
+
+        createDomElements(key, content);
+
+    }
+}
+
+/*used inside displayQuote() it creates HTML elements and */
+function createDomElements(key, content) {
+    var newDomElement;
+
+
+    createDomElement(key, content, "p", "quote", "quote-box");
+    createDomElement(key, content, "p", "tags", "quote-box");
+    createDomElement(key, content, "span", "author", "source");
+    createDomElement(key, content, "span", "citation", "source");
+    createDomElement(key, content, "span", "year", "source");
+
+
+}
+
+//creates a dom element using (key and content) and appends it as a child to the especified parent Element
+function createDomElement(key, content, newElementType, conditionKey, parentElementID) {
+    if (key == conditionKey) {
+        if (document.getElementById(key)) {
+            document.getElementById(key).remove();
+        }
+        newDomElement = document.createElement(newElementType);
+        newDomElement.textContent = content;
+        newDomElement.className = key;
+        newDomElement.setAttribute('id', key);
+        document.getElementById(parentElementID).appendChild(newDomElement);
+    }
+}
+
+//print's the quote and makes the background blueish'
+function printQuote() {
+    filterQuote();
+    displayQuote();
+    randomBlueish("randomBlueish");
+}
+
+//It starts the first quote
+printQuote();
+
+//quote is printed every 30 seconds
+var intervalID = window.setInterval(printQuote, 30000);
+
+//prints a every time printQuote buttons is cliqued
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
